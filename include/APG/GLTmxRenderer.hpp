eee48a07ea7e0321065f9b34d17940c2a02de52e
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-12-27 14:12:16 +0000

    Add skeleton GL TMX rendering files

---
+++ include/APG/GLTmxRenderer.hpp
@@ -0,0 +1,32 @@
+/*
+ * GLTmxRenderer.hpp
+ * Copyright (C) 2014 Ashley Davis (SgtCoDFish)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef GLTMXRENDERER_HPP_
+#define GLTMXRENDERER_HPP_
+
+namespace APG {
+
+class GLTmxRenderer {
+private:
+public:
+};
+
+}
+
+#endif /* GLTMXRENDERER_HPP_ */
