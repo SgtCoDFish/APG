b3a5299f2dad9873f87c76f4ec544a74ce954b8a
Ashley Davis <ashley_davis10419@hotmail.com>
2018-04-20 11:24:26 +0100

    Fix namespacing + formatting for sdlfontmanager

--- include/APG/font/SDLFontManager.hpp
+++ include/APG/font/SDLFontManager.hpp
@@ -1,30 +1,3 @@
-/*
- * Copyright (c) 2015 See AUTHORS file.
- * All rights reserved.
-
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of the <organization> nor the
- *      names of its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef INCLUDE_APG_FONT_SDLFONTMANAGER_HPP_
 #define INCLUDE_APG_FONT_SDLFONTMANAGER_HPP_
 
@@ -40,6 +13,7 @@
 #include "APG/SXXDL.hpp"
 #include "APG/graphics/Texture.hpp"
 #include "APG/graphics/Sprite.hpp"
+#include "APG/graphics/PackedTexture.hpp"
 #include "APG/font/FontManager.hpp"
 
 namespace APG {
@@ -48,16 +22,19 @@ class SpriteBase;
 class SDLFontManager : public FontManager {
 public:
 	explicit SDLFontManager();
-	virtual ~SDLFontManager() = default;
 
-	virtual font_handle loadFontFile(const std::string &filename, int pointSize) override;
-	virtual void freeFont(font_handle &handle) override;
+	~SDLFontManager() override = default;
 
-	virtual void setFontColor(const font_handle &handle, const glm::vec4 &color) override;
+	font_handle loadFontFile(const std::string &filename, int pointSize) override;
 
-	virtual glm::ivec2 estimateSizeOf(const font_handle &fontHandle, const std::string &text) override;
-	virtual SpriteBase *renderText(const font_handle &fontHandle, const std::string &text, bool ignoreWhitespace = true,
-	        const FontRenderMethod method = FontRenderMethod::FAST) override;
+	void freeFont(font_handle &handle) override;
+
+	void setFontColor(const font_handle &handle, const glm::vec4 &color) override;
+
+	glm::ivec2 estimateSizeOf(const font_handle &fontHandle, const std::string &text) override;
+
+	SpriteBase *renderText(const font_handle &fontHandle, const std::string &text, bool ignoreWhitespace = true,
+						   FontRenderMethod method = FontRenderMethod::FAST) override;
 
 private:
 	static constexpr int MAX_OWNED_TEXTURES = 5;
@@ -65,9 +42,9 @@ private:
 
 	struct StoredFont {
 		StoredFont(const font_handle &handle, SXXDL::ttf::font_ptr &&ptr) :
-				        handle { handle },
-				        ptr { std::move(ptr) },
-				        color { 0, 0, 0, 255 } {
+				handle{handle},
+				ptr{std::move(ptr)},
+				color{0, 0, 0, 255} {
 		}
 
 		font_handle handle;
@@ -76,9 +53,9 @@ private:
 	};
 
 	struct StoredSDLText {
-		StoredSDLText(const std::string &text, const int spriteID) :
-				        text { text },
-				        spriteID { spriteID } {
+		StoredSDLText(std::string text, int spriteID) :
+				text{std::move(text)},
+				spriteID{spriteID} {
 		}
 
 		const std::string text;
@@ -90,12 +67,16 @@ private:
 	std::array<std::unique_ptr<Sprite>, MAX_OWNED_SPRITES> ownedSprites;
 
 	int findAvailableOwnedTexture(SDL_Surface *surface) const;
+
 	int findAvailableSpriteSlot() const;
 
 	SDL_Color glmToSDLColor(const glm::vec4 &glmColor);
 
-	SpriteBase * renderTextIgnoreWhitespace(const StoredFont &font, const std::string &text, const FontRenderMethod method, el::Logger * const logger);
-	SpriteBase * renderTextWithWhitespace(const StoredFont &font, const std::string &text, const FontRenderMethod method, el::Logger * const logger);
+	SpriteBase *renderTextIgnoreWhitespace(const StoredFont &font, const std::string &text, FontRenderMethod method,
+										   el::Logger *logger);
+
+	SpriteBase *renderTextWithWhitespace(const StoredFont &font, const std::string &text, FontRenderMethod method,
+										 el::Logger *logger);
 };
 
 }
--- src/font/SDLFontManager.cpp
+++ src/font/SDLFontManager.cpp
@@ -15,14 +15,15 @@
 
 #include "easylogging++.h"
 
+namespace APG {
+
 const int APG::SDLFontManager::MAX_OWNED_TEXTURES;
 const int APG::SDLFontManager::MAX_OWNED_SPRITES;
 
-APG::SDLFontManager::SDLFontManager() {
-
+SDLFontManager::SDLFontManager() {
 }
 
-APG::FontManager::font_handle APG::SDLFontManager::loadFontFile(const std::string &filename, int pointSize) {
+FontManager::font_handle SDLFontManager::loadFontFile(const std::string &filename, int pointSize) {
 	auto logger = el::Loggers::getLogger("APG");
 	auto sdlFont = SXXDL::ttf::make_font_ptr(TTF_OpenFont(filename.c_str(), pointSize));
 
@@ -40,14 +41,14 @@ APG::FontManager::font_handle APG::SDLFontManager::loadFontFile(const std::strin
 	}
 }
 
-void APG::SDLFontManager::freeFont(font_handle &handle) {
+void SDLFontManager::freeFont(font_handle &handle) {
 	loadedFonts.erase(handle);
 	freeFontHandle(handle);
 
 	handle = -1;
 }
 
-void APG::SDLFontManager::setFontColor(const font_handle &handle, const glm::vec4 &color) {
+void SDLFontManager::setFontColor(const font_handle &handle, const glm::vec4 &color) {
 	const auto &font = loadedFonts.find(handle);
 
 	REQUIRE(font != loadedFonts.end(), "Can't change color of a font that doesn't exist!");
@@ -55,7 +56,7 @@ void APG::SDLFontManager::setFontColor(const font_handle &handle, const glm::vec
 	font->second.color = glmToSDLColor(color);
 }
 
-glm::ivec2 APG::SDLFontManager::estimateSizeOf(const font_handle &fontHandle, const std::string &text) {
+glm::ivec2 SDLFontManager::estimateSizeOf(const font_handle &fontHandle, const std::string &text) {
 	const auto &font = loadedFonts.find(fontHandle);
 
 	glm::ivec2 ret;
@@ -67,8 +68,8 @@ glm::ivec2 APG::SDLFontManager::estimateSizeOf(const font_handle &fontHandle, co
 	return ret;
 }
 
-APG::SpriteBase *APG::SDLFontManager::renderText(const font_handle &fontHandle, const std::string &text,
-bool ignoreWhitespace, const FontRenderMethod method) {
+SpriteBase *SDLFontManager::renderText(const font_handle &fontHandle, const std::string &text,
+									   bool ignoreWhitespace, const FontRenderMethod method) {
 	const auto logger = el::Loggers::getLogger("APG");
 
 	const auto &found = loadedFonts.find(fontHandle);
@@ -84,7 +85,7 @@ bool ignoreWhitespace, const FontRenderMethod method) {
 	}
 }
 
-SDL_Color APG::SDLFontManager::glmToSDLColor(const glm::vec4 &glmColor) {
+SDL_Color SDLFontManager::glmToSDLColor(const glm::vec4 &glmColor) {
 	SDL_Color ret;
 
 	ret.r = (uint8_t) (glmColor.r * 255.0f);
@@ -95,7 +96,7 @@ SDL_Color APG::SDLFontManager::glmToSDLColor(const glm::vec4 &glmColor) {
 	return ret;
 }
 
-int APG::SDLFontManager::findAvailableOwnedTexture(SDL_Surface * const surface) const {
+int SDLFontManager::findAvailableOwnedTexture(SDL_Surface *surface) const {
 	for (int i = 0; i < MAX_OWNED_TEXTURES; ++i) {
 		if (ownedTextures[i] == nullptr) {
 			return i;
@@ -105,7 +106,7 @@ int APG::SDLFontManager::findAvailableOwnedTexture(SDL_Surface * const surface) 
 	return -1;
 }
 
-int APG::SDLFontManager::findAvailableSpriteSlot() const {
+int SDLFontManager::findAvailableSpriteSlot() const {
 	for (int i = 0; i < MAX_OWNED_SPRITES; ++i) {
 		if (ownedSprites[i] == nullptr) {
 			return i;
@@ -115,29 +116,31 @@ int APG::SDLFontManager::findAvailableSpriteSlot() const {
 	return -1;
 }
 
-APG::SpriteBase * APG::SDLFontManager::renderTextIgnoreWhitespace(const APG::SDLFontManager::StoredFont &font,
-        const std::string &text, const APG::FontRenderMethod method, el::Logger * const logger) {
+SpriteBase *SDLFontManager::renderTextIgnoreWhitespace(const SDLFontManager::StoredFont &font,
+													   const std::string &text,
+													   const FontRenderMethod method,
+													   el::Logger *logger) {
 	SDL_Surface *tempSurface = nullptr;
 
 	switch (method) {
-	case FontRenderMethod::NICE: {
-		// renders to a nice RGBA surface so we don't have to mess with it
-		tempSurface = TTF_RenderUTF8_Blended(font.ptr.get(), text.c_str(), font.color);
-		break;
-	}
+		case FontRenderMethod::NICE: {
+			// renders to a nice RGBA surface so we don't have to mess with it
+			tempSurface = TTF_RenderUTF8_Blended(font.ptr.get(), text.c_str(), font.color);
+			break;
+		}
 
-	case FontRenderMethod::FAST: {
-		// renders to a palette so needs to be converted
-		tempSurface = TTF_RenderUTF8_Solid(font.ptr.get(), text.c_str(), font.color);
+		case FontRenderMethod::FAST: {
+			// renders to a palette so needs to be converted
+			tempSurface = TTF_RenderUTF8_Solid(font.ptr.get(), text.c_str(), font.color);
 
-		tempSurface = SDL_ConvertSurfaceFormat(tempSurface, SDL_PIXELFORMAT_RGBA8888, 0);
-		break;
-	}
+			tempSurface = SDL_ConvertSurfaceFormat(tempSurface, SDL_PIXELFORMAT_RGBA8888, 0);
+			break;
+		}
 	}
 
 	if (tempSurface == nullptr) {
 		logger->fatal("Couldn't render text string \"%v\" using font handle %v, error: %v", text, font.handle,
-		TTF_GetError());
+					  TTF_GetError());
 		return nullptr;
 	}
 
@@ -145,7 +148,7 @@ APG::SpriteBase * APG::SDLFontManager::renderTextIgnoreWhitespace(const APG::SDL
 
 	if (ownedTextureID == -1) {
 		logger->fatal("Ran out of space for textures for rendered text (max is %v textures). Time to refactor!",
-		        APG::SDLFontManager::MAX_OWNED_TEXTURES);
+					  APG::SDLFontManager::MAX_OWNED_TEXTURES);
 		return nullptr;
 	}
 
@@ -153,7 +156,7 @@ APG::SpriteBase * APG::SDLFontManager::renderTextIgnoreWhitespace(const APG::SDL
 
 	if (ownedSpriteID == -1) {
 		logger->fatal("Ran out of space for sprites for rendered text (max is %v sprites). Time to refactor!",
-		        APG::SDLFontManager::MAX_OWNED_SPRITES);
+					  APG::SDLFontManager::MAX_OWNED_SPRITES);
 		return nullptr;
 	}
 
@@ -166,8 +169,9 @@ APG::SpriteBase * APG::SDLFontManager::renderTextIgnoreWhitespace(const APG::SDL
 	return ownedSprites[ownedSpriteID].get();
 }
 
-APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont &font, const std::string &text,
-        const FontRenderMethod method, el::Logger * const logger) {
+SpriteBase *SDLFontManager::renderTextWithWhitespace(const StoredFont &font, const std::string &text,
+													 const FontRenderMethod method,
+													 el::Logger *logger) {
 	static const std::string wsDelimiter = "\n";
 
 	std::vector<std::string> stringVector;
@@ -177,50 +181,50 @@ APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont
 	auto end = text.find(wsDelimiter);
 
 	while (end != std::string::npos) {
-		stringVector.emplace_back(text.substr(begin, end-begin));
+		stringVector.emplace_back(text.substr(begin, end - begin));
 		begin = end + wsDelimiter.length();
 		end = text.find(wsDelimiter, begin);
 	}
 
-	if(stringVector.empty()) {
+	if (stringVector.empty()) {
 		return renderTextIgnoreWhitespace(font, text, method, logger);
 	}
 
 	switch (method) {
-	case FontRenderMethod::NICE: {
-		for (const auto &s : stringVector) {
-			// renders to a nice RGBA surface so we don't have to mess with it
-			auto surface = TTF_RenderUTF8_Blended(font.ptr.get(), s.c_str(), font.color);
+		case FontRenderMethod::NICE: {
+			for (const auto &s : stringVector) {
+				// renders to a nice RGBA surface so we don't have to mess with it
+				auto surface = TTF_RenderUTF8_Blended(font.ptr.get(), s.c_str(), font.color);
 
-			if (surface == nullptr) {
-				logger->fatal("Couldn't render text string \"%v\" (%v of %v) using font handle %v. Error: %v", s,
-				        surfaces.size() + 1, stringVector.size(), font.handle, TTF_GetError());
-				return nullptr;
+				if (surface == nullptr) {
+					logger->fatal("Couldn't render text string \"%v\" (%v of %v) using font handle %v. Error: %v", s,
+								  surfaces.size() + 1, stringVector.size(), font.handle, TTF_GetError());
+					return nullptr;
+				}
+
+				surfaces.emplace_back(SXXDL::make_surface_ptr(surface));
 			}
-
-			surfaces.emplace_back(SXXDL::make_surface_ptr(surface));
-		}
-		break;
-	}
-
-	case FontRenderMethod::FAST: {
-		for (const auto &s : stringVector) {
-			// renders to a palette so needs to be converted
-			auto surface = TTF_RenderUTF8_Solid(font.ptr.get(), s.c_str(), font.color);
-
-			if (surface == nullptr) {
-				logger->fatal("Couldn't render text string \"%v\" (%v of %v) using font handle %v. Error: %v", s,
-				        surfaces.size() + 1, stringVector.size(), font.handle, TTF_GetError());
-				return nullptr;
-			}
-
-			surface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
-
-			surfaces.emplace_back(SXXDL::make_surface_ptr(surface));
+			break;
 		}
 
-		break;
-	}
+		case FontRenderMethod::FAST: {
+			for (const auto &s : stringVector) {
+				// renders to a palette so needs to be converted
+				auto surface = TTF_RenderUTF8_Solid(font.ptr.get(), s.c_str(), font.color);
+
+				if (surface == nullptr) {
+					logger->fatal("Couldn't render text string \"%v\" (%v of %v) using font handle %v. Error: %v", s,
+								  surfaces.size() + 1, stringVector.size(), font.handle, TTF_GetError());
+					return nullptr;
+				}
+
+				surface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
+
+				surfaces.emplace_back(SXXDL::make_surface_ptr(surface));
+			}
+
+			break;
+		}
 	}
 
 	auto maxWidth = 0, totalHeight = 0;
@@ -234,19 +238,20 @@ APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont
 	}
 
 	SDL_Surface *tempSurface = SDL_CreateRGBSurface(0, maxWidth, totalHeight, surfaces.front()->format->BitsPerPixel,
-	        surfaces.front()->format->Rmask, surfaces.front()->format->Gmask, surfaces.front()->format->Bmask,
-	        surfaces.front()->format->Amask);
+													surfaces.front()->format->Rmask, surfaces.front()->format->Gmask,
+													surfaces.front()->format->Bmask,
+													surfaces.front()->format->Amask);
 
 	if (tempSurface == nullptr) {
 		logger->fatal("Couldn't create master surface for multiline rendering with handle %v. Error: %v", font.handle,
-		        SDL_GetError());
+					  SDL_GetError());
 		return nullptr;
 	}
 
 	auto hCounter = 0;
 	for (const auto &surf : surfaces) {
-		auto rect = SDL_Rect { 0u, hCounter, 0u, 0u };
-		if(SDL_BlitSurface(surf.get(), nullptr, tempSurface, &rect) != 0) {
+		auto rect = SDL_Rect{0u, hCounter, 0u, 0u};
+		if (SDL_BlitSurface(surf.get(), nullptr, tempSurface, &rect) != 0) {
 			logger->fatal("Couldn't blit micro surface to multiline master surface, error: %v", SDL_GetError());
 			return nullptr;
 		}
@@ -258,7 +263,7 @@ APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont
 
 	if (ownedTextureID == -1) {
 		logger->fatal("Ran out of space for textures for rendered text (max is %v textures). Time to refactor!",
-		        APG::SDLFontManager::MAX_OWNED_TEXTURES);
+					  APG::SDLFontManager::MAX_OWNED_TEXTURES);
 		return nullptr;
 	}
 
@@ -266,7 +271,7 @@ APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont
 
 	if (ownedSpriteID == -1) {
 		logger->fatal("Ran out of space for sprites for rendered text (max is %v sprites). Time to refactor!",
-		        APG::SDLFontManager::MAX_OWNED_SPRITES);
+					  APG::SDLFontManager::MAX_OWNED_SPRITES);
 		return nullptr;
 	}
 
@@ -277,6 +282,8 @@ APG::SpriteBase * APG::SDLFontManager::renderTextWithWhitespace(const StoredFont
 	ownedSprites[ownedSpriteID] = std::move(sprite);
 
 	return ownedSprites[ownedSpriteID].get();
+}
+
 }
 
 #endif
